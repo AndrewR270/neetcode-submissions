f1a2f3e77574559e5a265cfebdf7cd3860483729
Andrew Rafal <andrewrafal12@gmail.com>
2026-09-25 12:46:48 -0700

    Add: valid-sudoku - submission-4

---
+++ Data Structures & Algorithms/valid-sudoku/submission-4.py
@@ -0,0 +1,50 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        # Membership can be tracked with sets for unique
+        # Must track row, col, and 3x3 portions
+        # O(n^2) space allowed
+
+        # Go over unique rows
+        for r in range(len(board)):
+            seen = set()
+            for c in range(len(board)):
+                element = board[r][c]
+                if element == ".": continue
+                else:
+                    if element in seen: return False
+                    seen.add(element)
+
+        # Go over unique cols
+        for c in range(len(board)):
+            seen = set()
+            for r in range(len(board)):
+                element = board[r][c]
+                if element == ".": continue
+                else:
+                    if element in seen: return False
+                    seen.add(element)
+
+        # Go over 3x3
+        for gridX in range(3):
+            for gridY in range(3):
+                seen = set()
+                for r in range(3):
+                    for c in range(3):
+                        element = board[r + 3*gridX][c + 3*gridY]
+                        if element == ".": continue
+                        else:
+                            if element in seen: return False
+                            seen.add(element)
+
+        return True
+
+board=[
+    [".",".",".",".","5",".",".","1","."],
+    [".","4",".","3",".",".",".",".","."],
+    [".",".",".",".",".","3",".",".","1"],
+    ["8",".",".",".",".",".",".","2","."],
+    [".",".","2",".","7",".",".",".","."],
+    [".","1","5",".",".",".",".",".","."],
+    [".",".",".",".",".","2",".",".","."],
+    [".","2",".","9",".",".",".",".","."],
+    [".",".","4",".",".",".",".",".","."]]
